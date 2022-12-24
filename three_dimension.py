fed42489e4d4f9f9c3d51393b8c8c8fbc8c8f3c7
Phil Yuan <yyccmx@gmail.com>
2022-12-23 21:52:01 -0600

    updated 3d_visualization

--- three_dimension.py
+++ three_dimension.py
@@ -1,29 +1,26 @@
-# adjust
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import pandas as pd  #导入pandas库
+import pandas as pd
 
 df = pd.read_excel("../1.xlsx")
-Z = np.array(df)  #方法三
-xx = np.arange(3, 4.1, step=0.2)    # X轴的坐标
-yy = [0.2, 0.22, 0.24, 0.26, 0.28, 0.30, 0.32, 0.34, 0.36, 0.38, 0.40]         # Y轴的坐标
+Z = np.array(df)
+xx = np.arange(3, 4.1, step=0.2)    # the coordinates of the X axis
+yy = [0.2, 0.22, 0.24, 0.26, 0.28, 0.30, 0.32, 0.34, 0.36, 0.38, 0.40]         # the coordinates of the Y axis
 
 print(xx)
 print(yy)
 print(Z)
 
-X, Y = np.meshgrid(xx, yy)#网格化坐标
-# # X, Y=xx.ravel(), yy.ravel()#矩阵扁平化
-#
+X, Y = np.meshgrid(xx, yy)          # Grid coordinates
+# # X, Y=xx.ravel(), yy.ravel()     # Matrix flattening
 fig = plt.figure()
 ax3 = plt.axes(projection='3d')
 
 ax3.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow')
-# ax3.contour(X, Y, Z, zdim='z', offset=0.079, cmap='rainbow')   #等高线图，要设置offset，为Z的最小值
+# ax3.contour(X, Y, Z, zdim='z', offset=0.079, cmap='rainbow')   # Contour map, assign the minimum value of Z to argument offset 
 
-#坐标轴设置
+# Set axis
 new_ticks = np.linspace(0.20, 0.40, 11)
 print(new_ticks)
 plt.yticks(new_ticks)
@@ -37,15 +34,15 @@ ax3.set_ylabel(' ', font2)
 ax3.set_zlabel(' ', font2)
 plt.show()
 
-# bottom=np.zeros_like(X)#设置柱状图的底端位值
-# Z=Z.ravel()#扁平化矩阵
+# bottom=np.zeros_like(X)   # Set the bottom level value of the histogram
+# Z=Z.ravel()               # Matrix flattening
 #
-# width = 0.1 #每一个柱子的长和宽
+# width = 0.1       # Width and height of every column
 # height = 0.01
 #
-# #绘图设置
+# Drawing settings
 # fig=plt.figure()
-# ax=fig.gca(projection='3d')#三维坐标轴
-# ax.bar3d(X, Y, bottom, width, height, Z, shade=True)#
+# ax=fig.gca(projection='3d')   # 3D axis
+# ax.bar3d(X, Y, bottom, width, height, Z, shade=True)
 
 # plt.show()
